24aa63c209594933369aafa6be17fd6164c567c4
pminervini <p.minervini@gmail.com>
2015-01-27 16:53:01 +0100

    update

---
+++ site/public/img/sfondi/cri/k.sh
@@ -0,0 +1,10 @@
+convert IMG_2868.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_2868.jpg
+convert IMG_2904.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_2904.jpg
+convert IMG_2937.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_2937.jpg
+convert IMG_3033.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_3033.jpg
+convert IMG_4030.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_4030.jpg
+convert IMG_4164.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_4164.jpg
+convert IMG_4171.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_4171.jpg
+convert IMG_9044.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_9044.jpg
+convert IMG_9106.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_9106.jpg
+convert IMG_9166.jpg  -strip -interlace Plane -gaussian-blur 0.05 -quality 85% c_IMG_9166.jpg
